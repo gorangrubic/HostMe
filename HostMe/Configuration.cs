d8b777ec18de8584d9060be58f029c02aa8d89c4
hardyVeles <hardy@veles.rs>
2017-12-11 04:27:09 +0100

    - implemented doAutoSelectUnusedPort and doAutoOpenDefaultBrowser functions (in Program.cs and Configuration.cs) - added default demo content for the site

--- HostMe/Configuration.cs
+++ HostMe/Configuration.cs
@@ -2,7 +2,41 @@
 {
     public class Configuration
     {
+        /// <summary>
+        /// Directory to host (with web files, i.e. index.html)
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
         public string Path { get; set; }
+
+
+        /// <summary>
+        /// Default port to serve
+        /// </summary>
+        /// <value>
+        /// The port.
+        /// </value>
         public int Port { get; set; }
+
+
+        /// <summary>
+        /// If the <see cref="Port"/> is already served at the localhost, it will pick next that is not served and use it instead
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [do automatic select unused port]; otherwise, <c>false</c>.
+        /// </value>
+        public bool doAutoSelectUnusedPort { get; set; } = true;
+
+
+        /// <summary>
+        /// If <c>true</c> it will try to open localhost at served port with OS default web broser
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [do automatic open default browser]; otherwise, <c>false</c>.
+        /// </value>
+        public bool doAutoOpenDefaultBrowser { get; set; } = true;
+
+
     }
 }
--- HostMe/Program.cs
+++ HostMe/Program.cs
@@ -5,6 +5,12 @@ using System.Reflection;
 using log4net;
 using MicroService4Net;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace HostMe
 {
@@ -14,22 +20,65 @@ namespace HostMe
         private const string CONFIG_FILE_NAME = "HostMe.config.json";
         private static readonly ILog Logger = HostMe.Logger.GetLogger();
 
+        private static String[] arguments = new String[] {};
+        private static MicroService service = null;
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             WritePassedArgsToLog(args);
+
+            arguments = args;
+
 
             var configuration = GetConfiguration() ?? new Configuration { Port = DEFAULT_PORT };
 
             StaticContentController.SiteRootPath = PathNormalizer.NormalizePath(configuration.Path);
             var port = configuration.Port;
 
+            if (configuration.doAutoSelectUnusedPort)
+            {
+                port = GetOpenPort(port);
+            }
+
+
+
             Logger.InfoFormat("Starting...\r\nPort = {0}\r\nPath = {1}",port,StaticContentController.SiteRootPath);
 
             var serviceName = Assembly.GetEntryAssembly().GetName().Name + "_Port_" + port;
-            var microService = new MicroService(port, serviceName, serviceName);
-            microService.Run(args);
+
+
+
+
+
+            service = new MicroService(port, serviceName, serviceName);
+
+            Thread secondThread = new Thread(runService);
+
+            secondThread.Start();
+
+           
+
+            if (configuration.doAutoOpenDefaultBrowser)
+            {
+                tryOpenUrl("http://localhost:" + port);
+            }
+
         }
 
+
+        public static void runService()
+        {
+            service.Run(arguments);
+
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// <returns></returns>
         private static Configuration GetConfiguration()
         {
             Configuration configuration = null;
@@ -58,6 +107,73 @@ namespace HostMe
 
             return configuration;
         }
+
+
+        /// <summary>
+        /// Gets the next open port
+        /// </summary>
+        /// <remarks>Based on Andreas Reiff's answer at https://stackoverflow.com/questions/679489/determine-if-port-is-in-use </remarks>
+        /// <param name="startPort">The start port.</param>
+        /// <returns></returns>
+        private static int GetOpenPort(int startPort = 2555)
+        {
+            int portStartIndex = startPort;
+            int count = 99;
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpEndPoints = properties.GetActiveTcpListeners();
+
+            List<int> usedPorts = tcpEndPoints.Select(p => p.Port).ToList<int>();
+            int unusedPort = 0;
+
+            unusedPort = Enumerable.Range(portStartIndex, 99).Where(port => !usedPorts.Contains(port)).FirstOrDefault();
+            return unusedPort;
+        }
+
+        /// <summary>
+        /// Tries the open URL.
+        /// </summary>
+        /// <remarks>
+        /// <param>Based on Joel Harkers' (Software Developer at Trancon BV, Brussel, België) answer on "How to open in default browser in C#" at StackOverFlow forum</param>
+        /// <param>Source: https://stackoverflow.com/questions/4580263/how-to-open-in-default-browser-in-c-sharp </param>
+        /// </remarks>
+        /// <param name="url">The URL that should be opened (if not the localhosted content)</param>
+        /// <returns></returns>
+        private static Boolean tryOpenUrl(String url)
+        {
+
+            Process proc = null;
+
+                try
+                {
+                    proc = Process.Start(url);
+                
+                }
+                catch (Exception ex)
+                {
+                    // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        url = url.Replace("&", "^&");
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        Process.Start("xdg-open", url);
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        Process.Start("open", url);
+                    }
+                    else
+                    {
+                    return false;
+                    }
+                }
+
+            return (proc != null);
+            
+        }
+
 
         private static string GetConfigAsJson(string configFilePath)
         {
